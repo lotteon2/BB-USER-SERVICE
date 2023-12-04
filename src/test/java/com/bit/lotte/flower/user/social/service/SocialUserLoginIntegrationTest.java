253aebb25799880daac4e4675cc7f902ae1ed4a6
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-04 16:51:09 +0900

    LF1-563 test: add the test about auth-logic

--- src/test/java/com/bit/lotte/flower/user/social/service/SocialUserLoginIntegrationTest.java
+++ src/test/java/com/bit/lotte/flower/user/social/service/SocialUserLoginIntegrationTest.java
@@ -2,10 +2,12 @@ package com.bit.lotte.flower.user.social.service;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.bit.lotte.flower.user.common.valueobject.AuthId;
 import com.bit.lotte.flower.user.social.dto.command.UserLoginCommand;
+import com.bit.lotte.flower.user.social.dto.response.UserDataResponse;
 import com.bit.lotte.flower.user.social.entity.SocialUser;
 import com.bit.lotte.flower.user.social.repository.SocialUserJpaRepository;
 import java.util.ArrayList;
@@ -54,31 +56,44 @@ class SocialUserLoginIntegrationTest {
 
   @DisplayName("유저가 존재하지 않을 때 유저 생성 테스트")
   @Test
-  void SocialUserLogin_WhenUserIsNotRegistered_CreateUser() {
+  void SocialUserLogin_WhenUserIsNotRegistered_CreateUserAndGetUserDataResponse() {
     Long userId = getCommand(testOauthId).getSocialId().getValue();
-    loginManager.process(getCommand(userId));
+    UserDataResponse response = loginManager.process(getCommand(userId));
     assertTrue(repository.findById(userId).isPresent());
+    assertNotNull(response);
+
   }
 
-  @DisplayName("유저가 존재하고 유저의 상태가 모두 isDeleted인 상태인 경우 새로운 계정 생성 후 ")
+  @DisplayName("유저가 존재하고 유저의 상태가 모두 isDeleted인 상태인 경우 새로운 계정 생성하는 테스트 ")
   @Test
-  void SocialUserLogin_WhenUserIsRegisteredAndSameIdUserIsAllDeleted_CreateNewUserWhichIsNotDeleted() {
+  void SocialUserLogin_WhenUserIsRegisteredAndSameIdUserIsAllDeleted_CreateNewUserWhichIsNotDeletedAndGetResponse() {
     List<SocialUser> deletedUsers = setAllUsersByStatus(true, testOauthId);
     repository.saveAll(deletedUsers);
+
     Long userId = getCommand(testOauthId).getSocialId().getValue();
     repository.save(saveSocialUserById(testOauthId));
-    loginManager.process(getCommand(userId));
+
+    UserDataResponse response = loginManager.process(getCommand(userId));
+
     List<SocialUser> notDeletedUsers = repository.findAllByOauthId(testOauthId)
         .stream()
         .filter(user -> !user.getIsDeleted())
         .collect(Collectors.toList());
-    assertEquals(1,notDeletedUsers.size());
+    assertEquals(1, notDeletedUsers.size());
+    assertNotNull(response);
 
   }
 
-  @DisplayName("유저가 존재하고 유저의 상태가 IsDeleted가 false인 경우 새로운 계정 생성을 하지 않는 테스트")
+  @DisplayName("유저가 존재하고 하나의 유저의 상태가 IsDeleted가 false인 경우 새로운 계정 생성을 하지 않는 테스트")
   @Test
-  void SocialUserLogin_WhenUserIsRegisteredAndThereIsNotDeletedUser_NotCreateUserSameSize() {
+  void SocialUserLogin_WhenUserIsRegisteredAndThereIsNotDeletedUser_NotCreateUserSameSizeAndGetResponse() {
+    repository.saveAll(setTwoUserOneUserIsNotDeletedStatus(testOauthId));
+    int sizeBeforeInput = repository.findAllByOauthId(testOauthId).size();
+    UserDataResponse response = loginManager.process(getCommand(testOauthId));
+    int sizeAfterLoginProcess = repository.findAllByOauthId(testOauthId).size();
+
+    assertEquals(sizeBeforeInput, sizeAfterLoginProcess);
+    assertNotNull(response);
 
   }
 
@@ -107,6 +122,30 @@ class SocialUserLoginIntegrationTest {
     return deletedUsers;
   }
 
+  private List<SocialUser> setTwoUserOneUserIsNotDeletedStatus(Long oauthId) {
+    List<SocialUser> deletedUsers = new ArrayList<>();
+    SocialUser user1 = SocialUser.builder()
+        .id(1L)
+        .oauthId(oauthId)
+        .nickname("User1")
+        .profileImage("profile1.jpg")
+        .phoneNumber("1234567890")
+        .isDeleted(false)
+        .build();
+    deletedUsers.add(user1);
+
+    SocialUser user2 = SocialUser.builder()
+        .id(2L)
+        .oauthId(oauthId)
+        .nickname("User2")
+        .profileImage("profile2.jpg")
+        .phoneNumber("9876543210")
+        .isDeleted(true)
+        .build();
+    deletedUsers.add(user2);
+
+    return deletedUsers;
+  }
 
 
   private SocialUser saveSocialUserById(Long id) {
