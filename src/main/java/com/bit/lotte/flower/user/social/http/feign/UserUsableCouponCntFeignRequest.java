ca40f7b54093af14e1674826e2e9eba26d5c142b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-09 21:47:06 +0900

    add the request mapping at feign client

--- src/main/java/com/bit/lotte/flower/user/social/http/feign/UserUsableCouponCntFeignRequest.java
+++ src/main/java/com/bit/lotte/flower/user/social/http/feign/UserUsableCouponCntFeignRequest.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "get-user-usable-coupon-cnt", value = "${service.coupon.domain}")
+@RequestMapping("${service.coupon.domain}")
+@FeignClient(name = "get-user-usable-coupon-cnt")
 public interface UserUsableCouponCntFeignRequest {
   @GetMapping("/coupons/count")
   ResponseEntity<Long> getUsableUserCouponCnt(@RequestHeader Long userId);
