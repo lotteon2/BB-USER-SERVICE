9c32290e0e658dc9c9c8ad843ff920e7bfe6ccab
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-20 18:10:39 +0900

    fix: fix the requestData has the CreatedAt value

--- src/main/java/com/bit/lotte/flower/user/admin/service/GetStoreManagerApplicationService.java
+++ src/main/java/com/bit/lotte/flower/user/admin/service/GetStoreManagerApplicationService.java
@@ -3,18 +3,13 @@ package com.bit.lotte.flower.user.admin.service;
 
 import com.bit.lotte.flower.user.admin.dto.StoreManagerApplicationData;
 import com.bit.lotte.flower.user.admin.dto.response.StoreManagerApplicationFormResponse;
-import com.bit.lotte.flower.user.common.valueobject.StoreId;
-import com.bit.lotte.flower.user.common.valueobject.StoreManagerStatus;
-import com.bit.lotte.flower.user.social.repository.FindSocialUserByLongIdService;
 import com.bit.lotte.flower.user.store.entity.StoreManager;
-import com.bit.lotte.flower.user.store.repository.StoreManagerJpaRepository;
 import com.bit.lotte.flower.user.store.service.FindStoreMangerService;
 import java.util.ArrayList;
 import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -43,7 +38,7 @@ public class GetStoreManagerApplicationService {
     for (StoreManager storeManager : managerList) {
       applicationDataList.add(
           StoreManagerApplicationData.builder().key(storeManager.getId())
-              .requestDate(storeManager.getUpdatedAt()).storeManagerName(storeManager.getName())
+              .storeManagerName(storeManager.getName())
               .requestDate(storeManager.getCreatedAt())
               .storeManagerBusinessNumber(storeManager.getBusinessNumberImage()).build());
     }
