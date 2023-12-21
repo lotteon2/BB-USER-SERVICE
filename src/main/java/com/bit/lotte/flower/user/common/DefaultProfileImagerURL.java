0d10313ae77fa5475684bfcdff02a37044f496f8
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-21 17:42:52 +0900

    Update DefaultProfileImagerURL.java

--- src/main/java/com/bit/lotte/flower/user/common/DefaultProfileImagerURL.java
+++ src/main/java/com/bit/lotte/flower/user/common/DefaultProfileImagerURL.java
@@ -4,5 +4,5 @@ package com.bit.lotte.flower.user.common;
 
 public class DefaultProfileImagerURL {
 
-public static String PROFILE_DEFAULT_IMAGE_URL ="https://ibb.co/BN1YNzw";
+public static String PROFILE_DEFAULT_IMAGE_URL ="https://i.ibb.co/DpqjpVL/Group-1000004120.png";
 }
