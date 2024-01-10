3a7a8e89d253566baf36d50b69b1dfa5c7fb43be
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-01-10 09:50:07 +0900

    chore: add the JPA Auditing annotation at top of the UserApplication

--- src/main/java/com/bit/lotte/flower/user/UserServiceApplication.java
+++ src/main/java/com/bit/lotte/flower/user/UserServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
 @EnableEurekaClient
 @EnableJpaRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class UserServiceApplication {
   public static void main(String[] args) {
--- src/main/java/com/bit/lotte/flower/user/social/http/message/GetUserLikesCntRequest.java
+++ src/main/java/com/bit/lotte/flower/user/social/http/message/GetUserLikesCntRequest.java
@@ -1,7 +1,7 @@
 package com.bit.lotte.flower.user.social.http.message;
 
 import com.bit.lotte.flower.user.common.valueobject.AuthId;
-import com.bit.lotte.flower.user.common.valueobject.UserId;
+import com.bit.lotte.flower.user.common.valueobject.AuthId;
 import org.springframework.stereotype.Component;
 
 @Component
